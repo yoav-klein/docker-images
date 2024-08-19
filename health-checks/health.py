024207bc6e9f3644f6a72d3c7fe4ec3224922414
Yoav Klein <yoavklein25@gmail.com>
2024-08-19 08:25:36 +0000

    Health check

---
+++ health-checks/health.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/health')
+def health_check():
+    return "OK"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8090)
+
