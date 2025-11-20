9820ed754d28265be7339611d0ba36ffb2eb6371
yoav <yoavklein25@gmail.com>
2025-11-20 18:42:32 +0000

    Java client and serverito

---
+++ java-server-and-client/client/app/src/main/java/org/example/Observability.java
@@ -0,0 +1,56 @@
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationHandler;
+import org.springframework.http.client.observation.ClientRequestObservationContext;
+
+
+
+@Configuration
+public class Observability {
+
+    @Bean
+    public AtomicInteger httpClientInFlightCounter(MeterRegistry registry) {
+        AtomicInteger inFlight = new AtomicInteger(0);
+
+        Gauge.builder("http.client.requests.in_flight", inFlight, AtomicInteger::get)
+             .description("Number of in-flight HTTP client requests")
+             .register(registry);
+        
+        System.out.println("httpClientInFlightCounter");
+
+        return inFlight;
+    }
+
+    @Bean
+    public ObservationHandler<ClientRequestObservationContext> clientInFlightHandler(
+            AtomicInteger httpClientInFlightCounter) {
+
+        return new ObservationHandler<>() {
+
+            @Override
+            public void onStart(ClientRequestObservationContext context) {
+                httpClientInFlightCounter.incrementAndGet();
+
+                System.out.println("clientInFlightHandler:: onStart");
+            }
+
+            @Override
+            public void onStop(ClientRequestObservationContext context) {
+                System.out.println("clientInFlightHandler:: onStop");
+                httpClientInFlightCounter.decrementAndGet();
+            }
+
+            @Override
+            public boolean supportsContext(Observation.Context context) {
+                //return context instanceof ClientRequestObservationContext;
+                return true;
+            }
+        };
+    }
+}
